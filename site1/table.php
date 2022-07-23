764d398e07e7f3825c21fad734183d1424287cc1
Ruslan Bikulov <bikulovrus90@gmail.ru>
2022-07-23 18:04:54 +0500

    23.07.2022

---
+++ site1/table.php
@@ -0,0 +1,163 @@
+
+<?php
+include_once 'inc/header.php';
+?>
+<table border="1">
+    <tr align="center">
+       <td width="100px" bgcolor="red">
+                <div class="one"><a href="https://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D1%82%D0%B8%D0%B9">Li</a> </div>
+                <div class="two">3</div>
+            <br>
+                <div class="thre">6,939</div>
+            <br>
+                <div class="four">Литий</div>
+        </td>
+
+        <td width="100px" bgcolor="red">
+            <div class="one"><a href="https://ru.wikipedia.org/wiki/%D0%91%D0%B5%D1%80%D0%B8%D0%BB%D0%BB%D0%B8%D0%B9">Be</a> </div>
+            <div class="two">4</div>
+            <br>
+            <div class="thre">9,0122</div>
+            <br>
+            <div class="four">Бериллий</div>
+        </td>
+
+        <td width="100px" bgcolor="yellow">
+            <div class="one"><a href="https://ru.wikipedia.org/wiki/%D0%91%D0%BE%D1%80_(%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82)">B</a> </div>
+            <div class="two">5</div>
+            <br>
+            <div class="thre">10,811</div>
+            <br>
+            <div class="four">Бор</div>
+        </td>
+
+        <td width="100px" bgcolor="yellow">
+            <div class="one"><a href="https://ru.wikipedia.org/wiki/%D0%A3%D0%B3%D0%BB%D0%B5%D1%80%D0%BE%D0%B4">C</a></div>
+            <div class="two">6</div>
+            <br>
+            <div class="thre">12,01115</div>
+            <br>
+            <div class="four">Углерод</div>
+        </td>
+    </tr>
+
+    <tr align="center">
+        <td width="100px" bgcolor="red">
+            <div class="one"><a href="https://ru.wikipedia.org/wiki/%D0%9D%D0%B0%D1%82%D1%80%D0%B8%D0%B9">Na</a> </div>
+            <div class="two">11</div>
+            <br>
+            <div class="thre">22,9898</div>
+            <br>
+            <div class="four">Натрий</div>
+        </td>
+
+        <td width="100px" bgcolor="red">
+            <div class="one"><a href="https://ru.wikipedia.org/wiki/%D0%9C%D0%B0%D0%B3%D0%BD%D0%B8%D0%B9">Mg</a> </div>
+            <div class="two">12</div>
+            <br>
+            <div class="thre">24,3122</div>
+            <br>
+            <div class="four">Магний</div>
+        </td>
+
+        <td width="100px" bgcolor="yellow">
+            <div class="one"><a href="https://ru.wikipedia.org/wiki/%D0%90%D0%BB%D1%8E%D0%BC%D0%B8%D0%BD%D0%B8%D0%B9">Al</a> </div>
+            <div class="two">13</div>
+            <br>
+            <div class="thre">26,9815</div>
+            <br>
+            <div class="four">Алюминий</div>
+        </td>
+
+        <td width="100px" bgcolor="yellow">
+            <div class="one"><a href="https://ru.wikipedia.org/wiki/%D0%9A%D1%80%D0%B5%D0%BC%D0%BD%D0%B8%D0%B9">Si</a> </div>
+            <div class="two">14</div>
+            <br>
+            <div class="thre">28,086</div>
+            <br>
+            <div class="four">Кремний</div>
+        </td>
+    </tr>
+
+    <tr align="center">
+        <td width="100px" bgcolor="red">
+            <div class="one"><a href="
+https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D0%BB%D0%B8%D0%B9">K</a> </div>
+            <div class="two">19</div>
+            <br>
+            <div class="thre">39,102</div>
+            <br>
+            <div class="four">Калий</div>
+        </td>
+
+        <td width="100px" bgcolor="red">
+            <div class="one"><a href="https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D0%BB%D1%8C%D1%86%D0%B8%D0%B9">Ca</a> </div>
+            <div class="two">20</div>
+            <br>
+            <div class="thre">40,08</div>
+            <br>
+            <div class="four">Кальций</div>
+        </td>
+
+        <td width="100px" bgcolor="yellow">
+            <div class="one"><a href="https://ru.wikipedia.org/wiki/%D0%A1%D0%BA%D0%B0%D0%BD%D0%B4%D0%B8%D0%B9">Sc</a> </div>
+            <div class="two">21</div>
+            <br>
+            <div class="thre">44,9561</div>
+            <br>
+            <div class="four">Скандий</div>
+        </td>
+
+        <td width="100px" bgcolor="yellow">
+            <div class="one"><a href="https://ru.wikipedia.org/wiki/%D0%A2%D0%B8%D1%82%D0%B0%D0%BD_(%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82)">Ti</a> </div>
+            <div class="two">22</div>
+            <br>
+            <div class="thre">47,90</div>
+            <br>
+            <div class="four">Титан</div>
+        </td>
+    </tr>
+
+    <tr align="center">
+        <td width="100px" bgcolor="red">
+            <div class="one"><a href="https://ru.wikipedia.org/wiki/%D0%9C%D0%B5%D0%B4%D1%8C#:~:text=%D0%9C%D0%B5%D0%B4%D1%8C%20(%D1%85%D0%B8%D0%BC%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B9%20%D1%81%D0%B8%D0%BC%D0%B2%D0%BE%D0%BB%20%E2%80%94%20Cu%2C,%D0%9C%D0%B5%D0%BD%D0%B4%D0%B5%D0%BB%D0%B5%D0%B5%D0%B2%D0%B0%2C%20%D1%81%20%D0%B0%D1%82%D0%BE%D0%BC%D0%BD%D1%8B%D0%BC%20%D0%BD%D0%BE%D0%BC%D0%B5%D1%80%D0%BE%D0%BC%2029.">Cu</a> </div>
+            <div class="two">29</div>
+            <br>
+            <div class="thre">63,546</div>
+            <br>
+            <div class="four">Медь</div>
+        </td>
+
+        <td width="100px" bgcolor="red">
+            <div class="one"><a href="
+https://ru.wikipedia.org/wiki/%D0%A6%D0%B8%D0%BD%D0%BA">Zn</a> </div>
+            <div class="two">30</div>
+            <br>
+            <div class="thre">65,37</div>
+            <br>
+            <div class="four">Цинк</div>
+        </td>
+
+        <td width="100px" bgcolor="yellow">
+            <div class="one"><a href="https://ru.wikipedia.org/wiki/%D0%93%D0%B0%D0%BB%D0%BB%D0%B8%D0%B9">Ga</a> </div>
+            <div class="two">31</div>
+            <br>
+            <div class="thre">69,72</div>
+            <br>
+            <div class="four">Галий</div>
+        </td>
+
+        <td width="100px" bgcolor="yellow">
+            <div class="one"><a href="https://ru.wikipedia.org/wiki/%D0%93%D0%B5%D1%80%D0%BC%D0%B0%D0%BD%D0%B8%D0%B9">Ge</a> </div>
+            <div class="two">32</div>
+            <br>
+            <div class="thre">72,59</div>
+            <br>
+            <div class="four">Германий</div>
+        </td>
+    </tr>
+
+</table>
+<?php
+include_once 'inc/footer.php';
+?>
